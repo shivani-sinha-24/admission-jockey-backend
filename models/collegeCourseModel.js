f03d8e37af7389480dbf3242d16ffd240a3632ec
kartikay_rana <kartik65361@gmail.com>
2023-07-08 18:46:32 +0530

    course addded issue is resolved

--- models/collegeCourseModel.js
+++ models/collegeCourseModel.js
@@ -3,39 +3,10 @@ import mongoose from 'mongoose';
 
 const collegeCourseSchema = new mongoose.Schema({
 
-
-    // name: {
-    //     type: String,
-    //     allowNull: false
-    // },
-    // full_name: {
-    //     type: String,
-    //     allowNull: false
-    // },
-    // duration: {
-    //     type: Number,
-    //     allowNull: false
-    // },
-    // type: {
-    //     type: String,
-    //     allowNull: false
-    // },
     fees: {
         type: Number,
         allowNull: false
     },
-    // category: {
-    //     type: String,
-    //     allowNull: false
-    // },
-    // sub_category: {
-    //     type: String,
-    //     allowNull: false
-    // },
-    // stream: {
-    //     type: String,
-    //     allowNull: false
-    // },
     CollegeID: {
         type: String,
         allowNull: false
@@ -47,16 +18,6 @@ const collegeCourseSchema = new mongoose.Schema({
     UniversityCourseID: {
         type: String,
     },
-    // lateral_entry: {
-    //     type: String
-    // },
-    // eligibilty: {
-    //     type: String
-    // },
-    // description: {
-    //     type: String,
-    //     allowNull: false
-    // },
 },
 
     { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
